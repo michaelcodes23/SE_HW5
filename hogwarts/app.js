7153bb3943c093bd02bd40b5150d181f4cd47627
Your Name <you@example.com>
2021-03-21 19:07:30 -0400

    Commit 0 - All my files are linked and loaded

---
+++ hogwarts/app.js
@@ -0,0 +1,8 @@
+// if (typeof jQuery == 'undefined'){
+//     console.log('oops! I Still have to link my jQuery properly!');
+// }
+// else{
+//     console.log('I did it! I linked jQuery and this js file!')
+// }
+// console.log("Homework 5 - Hogwarts!")
+//Commit 0 - All my files are linked and loaded!
