5cc869a1accecb4fa912bc62d36a55cee98cb8dc
michaelcodes23 <cuencamich@gmail.com>
2021-03-21 19:30:32 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -11,9 +11,19 @@ else{
     const $h1 = $('<h1>').text('Hogwarts');  
     // const $h1 = $('<h1>').text('Trois couleurs: rouge, blanc, et blue') 
     // $div1.append($h1);
+    const $name = $('<h2>').text('Michael')
+//Commit 1 - I added my first element and it was fun
+    const $house = $('<h3>').text('Gryffindor');
+    const $pet = $('<h4>').attr('class','dog').text('Jackie');
+    const $wand= $('<h4>').text('Birch Wand with Phoenix Feather Core');
+//Commit 2 - I added more elements. My power grows with my dedication and hard work
+
 $(() => {
 
     $(container).append($h1);
+    $(container).append($name);
+    $(container).append($house);
+    $(container).append($pet);
+    $(container).append($wand);
     console.log($container);
 });
-//Commit 1 - I added my first element and it was fun
